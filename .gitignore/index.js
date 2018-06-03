96f4212cf7ed918fb314b59db46e46880c18b181
MucusSorevegnty <39030321+MucusSorevegnty@users.noreply.github.com>
2018-06-03 13:05:07 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,19 @@
+/* BOT CONFIGURATION */
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = process.env.SECRET;
+const admins_id = [
+'285418210965323776'
+];
+const prefix = "az.";
+/*  /-- BOT CONFIGURATION --/  */
+
+bot.on('ready',  ()  =>  {
+      bot.user.setActivity(`az.help | ${bot.guilds.size} serveur | Par Azkun.`);
+})
+
+bot.login(token);
+ 
+message.content.toLowerCase().includes("merde"){
+message.reply("Évitez de dire une insulte ! Merci.")
+}
